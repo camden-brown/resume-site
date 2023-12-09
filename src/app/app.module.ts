65276f12dd39be35b9ea3e0a8834d4f088e65d3d
Camden Brown <camdenebrown@gmail.com>
2023-12-09 15:05:08 -0600

    remove old code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { TokenInterceptor } from './shared/interceptors/token.interceptor';
-import { LoadingComponent } from './shared/components/loading/loading.component';
+import { HttpClientModule } from '@angular/common/http';
 import { PreloadAllModules, RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routing';
 import { MatIconRegistry } from '@angular/material/icon';
@@ -18,18 +12,7 @@ import { MatIconRegistry } from '@angular/material/icon';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    LoadingComponent,
     RouterModule.forRoot(APP_ROUTES, { preloadingStrategy: PreloadAllModules }),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-    }),
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
